9948b1ae7bb2be453d956d76cbff33ad8449feec
Arthur Penha <penharthur@hotmail.com>
2022-02-17 18:23:55 -0300

    Transferência da página cadastroVeiculos (HTML e CSS)

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import CadastroCliente from './views/CadastroCliente'
 import CadastroLocadora from './views/CadastroLocadora'
 import MainPage from './views/MainPage'
 import Duvidas from './views/Duvidas'
+import CadastroVeiculos from './views/CadastroVeiculos'
 
 
 const AppRoutes = () => {
@@ -14,6 +15,7 @@ const AppRoutes = () => {
                 <Route path="/contatos" element={<Contato />} />
                 <Route path="/cadastrocliente" element={<CadastroCliente />} />
                 <Route path="/cadastrolocadora" element={<CadastroLocadora />} />
+                <Route path="/cadastroveiculos" element={<CadastroVeiculos />} />
                 <Route path="/duvidas" element={<Duvidas />} />
             </Routes>
         </BrowserRouter>
---
+++ src/views/CadastroVeiculos/index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import PageConstructor from '../../Components/PageConstructor';
+import './styles.css'
+
+function CadastroVeiculos() {
+    const [modeloDoVeiculo, setModeloDoVeiculo] = useState('')
+    const [renavam, setRenavam] = useState('')
+    const [placa, setPlaca] = useState('')
+    const [kmRodado, setKmRodado] = useState('')
+    const [custoDiaria, setCustoDiaria] = useState('')
+    const [status, setStatus] = useState('')
+    const [locadoraAtual, setLocadoraAtual] = useState('')
+    const [locadoraProprietaria, setLocadoraProprietaria] = useState('')
+    const [overlay, setOverlay] = useState(false)
+
+    return (
+        <>
+            <PageConstructor>
+                <div className="App">
+                    <main className="section">
+                        {
+                            overlay ? <div id="overlay">
+                                <div className="message">
+                                    <h2 style="color:#414D92"> Cadastro realizado com sucesso!</h2>
+                                    <button onClick={setOverlay(false)}> Ok</button>
+                                </div>
+                            </div>
+                                : ""
+                        }
+                        <h1>
+                            Cadastro de Veiculos
+                        </h1>
+                        <form name="dadosVeiculos">
+                            <div class="forms">
+                                <h2>Dados do Veículo</h2>
+                                <div class="formsvdivs">
+                                    <div class="formshdivs">
+                                        <label for="modeloveiculo"> Modelo do Veículo:</label>
+                                        <input type="text" name="modeloveiculo" id="modeloveiculo" value={modeloDoVeiculo} onChange={event => setModeloDoVeiculo(event.target.value)} required />
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="renavam"> RENAVAM:</label>
+                                        <input type="number" maxlength="11" name="renavam" id="renavam" value={renavam} onChange={event => setRenavam(event.target.value)} required />
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="placa"> Placa:</label>
+                                        <input type="text" maxlength="7" name="placa" id="placa" value={placa} onChange={event => setPlaca(event.target.value)} required />
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="kmrodado"> KM Rodados:</label>
+                                        <input type="number" name="kmrodado" id="kmrodado" value={kmRodado} onChange={event => setKmRodado(event.target.value)} required />
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="custodiaria"> Custo da diária</label>
+                                        <input type="number" name="custodiaria" id="custodiaria" value={custoDiaria} onChange={event => setCustoDiaria(event.target.value)} required />
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="status"> Status: </label>
+                                        <select name="status" value={status} onChange={event => setStatus(event.target.value)} ></select>
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="localAtual"> Locadora Atual:</label>
+                                        <select name="localAtual" value={locadoraAtual} onChange={event => setLocadoraAtual(event.target.value)} ></select>
+                                    </div>
+                                    <div class="formshdivs">
+                                        <label for="localProprietaria"> Locadora Propietária:</label>
+                                        <select name="localProprietaria" value={locadoraProprietaria} onChange={event => setLocadoraProprietaria(event.target.value)} ></select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="formsbuttons">
+                                <button>Cancelar</button><button>Salvar</button>
+                            </div>
+                        </form>
+                    </main>
+                </div>
+            </PageConstructor>
+        </>
+    )
+}
+
+export default CadastroVeiculos
