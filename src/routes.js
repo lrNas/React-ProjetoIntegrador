6de9f0eee7cc74d3a876f570c9bfc9f28d589811
Arthur Penha <penharthur@hotmail.com>
2022-02-17 17:07:19 -0300

    Ajuste routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Contato from './views/Contato'
 import CadastroCliente from './views/CadastroCliente'
 import MainPage from './views/MainPage'
@@ -6,16 +6,16 @@ import Duvidas from './views/Duvidas'
 
 
 const AppRoutes = () => {
-    return(
-        <BrowserRouter>   
-        <Routes>
-        <Route  path= "/"  element = {<MainPage/>} />
-        <Route  path= "/contatos"  element = {<Contato />} />
-        <Route  path= "/cadastrocliente"  element = {<CadastroCliente />} />
-        <Route  path= "/duvidas"  element = {<Duvidas />} />
-        </Routes>  
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/contatos" element={<Contato />} />
+                <Route path="/cadastrocliente" element={<CadastroCliente />} />
+                <Route path="/duvidas" element={<Duvidas />} />
+            </Routes>
         </BrowserRouter>
     )
 }
 
-export	default AppRoutes
+export default AppRoutes
