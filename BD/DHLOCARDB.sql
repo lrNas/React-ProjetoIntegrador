ed6ad6d176d3b05c119bedb3c617f532efed786b
Lucas Rodrigues do Nascimento <91696059+lrNas@users.noreply.github.com>
2022-03-13 19:39:17 -0300

    Normalização de nomes
    Retirados Ç, nomes na tabela no plural com S (quando não fica no plural só adicionando um S, nome vai para inglês), reduzido redundância de texto e diminuido nome das colunas.

--- BD/DHLOCARDB.sql
+++ BD/DHLOCARDB.sql
@@ -1,97 +1,98 @@
 CREATE SCHEMA dhlocar;
 use dhlocar;
 
-CREATE TABLE tipo_usuario( id_tipo_usuario INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE tipo_usuarios( 
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 descricao VARCHAR(100) NOT NULL);
 
 
     
-    CREATE TABLE endereco (
-	id_endereco INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	cep_endereco INT(8) NOT NULL,
-    logadouro_endereco VARCHAR(100) NOT NULL,
-    complemento_endereco VARCHAR(100) NOT NULL,
-    estado_endereco VARCHAR(20) NOT NULL,
-    cidade_endereco VARCHAR(30) NOT NULL
+    CREATE TABLE enderecos (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	cep INT(8) NOT NULL,
+    logadouro VARCHAR(100) NOT NULL,
+    complemento VARCHAR(100) NOT NULL,
+    estado VARCHAR(20) NOT NULL,
+    cidade VARCHAR(30) NOT NULL
     );
 
-CREATE TABLE usuario (
-	id_usuario INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE usuarios (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	fk_id_tipo_usuario INT(10) NOT NULL,
-    nome_completo_usuario VARCHAR(100) NOT NULL,
-    email_usuario VARCHAR(100) NOT NULL UNIQUE,
-    senha_usuario VARCHAR(100) NOT NULL UNIQUE,
-    cpf_usuario INT(10) NOT NULL UNIQUE,
-    telefone_usuario INT(11) NOT NULL,
-    data_nascimento_usuario DATE NOT NULL,
-    cnh_usuario INT(10) NOT NULL UNIQUE,
-    validade_cnh_usuario DATE NOT NULL,
+    nome_completo VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL UNIQUE,
+    cpf INT(10) NOT NULL UNIQUE,
+    telefone INT(11) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    cnh INT(10) NOT NULL UNIQUE,
+    validade_cnh DATE NOT NULL,
     fk_id_endereco INT(10) NOT NULL,
-    foreign key (fk_id_endereco) references dhlocar.endereco(id_endereco),
-    foreign key (fk_id_tipo_usuario) references dhlocar.tipo_usuario(id_tipo_usuario)
+    foreign key (fk_id_endereco) references dhlocar.enderecos(id),
+    foreign key (fk_id_tipo_usuario) references dhlocar.tipo_usuarios(id)
     
     );
 
-    CREATE TABLE cartao (
-	id_cartao INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome_cartao VARCHAR(45) NOT NULL,
-    numero_cartao INT(16) NOT NULL UNIQUE,
-    validade_cartao DATE NOT NULL,
-    cvc_cartao INT(3) NOT NULL,
+    CREATE TABLE cards (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45) NOT NULL,
+    numero INT(16) NOT NULL UNIQUE,
+    validade DATE NOT NULL,
+    cvc INT(3) NOT NULL,
     fk_id_usuario INT(10) NOT NULL,
-	foreign key (fk_id_usuario) references dhlocar.usuario(id_usuario)
+	foreign key (fk_id_usuario) references dhlocar.usuarios(id)
     );
     
-    CREATE TABLE locadora (
-	id_locadora INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome_locadora VARCHAR(100) NOT NULL,
-    email_locadora VARCHAR(100) NOT NULL UNIQUE,
-    cnpj_locadora INT(14) NOT NULL UNIQUE,
-    telefone_locadora INT(11) NOT NULL UNIQUE,
+    CREATE TABLE locadoras (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cnpj INT(14) NOT NULL UNIQUE,
+    telefone INT(11) NOT NULL UNIQUE,
     fk_id_endereco INT(10) NOT NULL,
-    foreign key (fk_id_endereco) references dhlocar.endereco(id_endereco)
+    foreign key (fk_id_endereco) references dhlocar.enderecos(id)
     );
     
-	CREATE TABLE status_veiculo (
-	id_status INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	CREATE TABLE status_veiculos (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     descricao VARCHAR(100) NOT NULL
     );
     
-    CREATE TABLE mensagem (
-	id_mensagem INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome_completo_mensagem VARCHAR(100) NOT NULL,
-    email_mensagem VARCHAR(100) NOT NULL,
-    conteudo_mensagem VARCHAR(1000) NOT NULL
+    CREATE TABLE messages (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_completo VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    conteudo VARCHAR(1000) NOT NULL
     );
     
-	CREATE TABLE veiculo (
-	id_veiculo INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    modelo_veiculo VARCHAR(100) NOT NULL,
-    placa_veiculo VARCHAR(10) NOT NULL UNIQUE,
-    km_rodados_veiculo DECIMAL(7,2) NOT NULL,
-    custo_diaria_veiculo DECIMAL (5,2)  NOT NULL,
-    renavam_veiculo INT(11) NOT NULL UNIQUE,
-    data_ultima_atualizacao_veiculo DATE NOT NULL,
+	CREATE TABLE veiculos (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    modelo VARCHAR(100) NOT NULL,
+    placa VARCHAR(10) NOT NULL UNIQUE,
+    km_rodados DECIMAL(7,2) NOT NULL,
+    custo_diaria DECIMAL (5,2)  NOT NULL,
+    renavam INT(11) NOT NULL UNIQUE,
+    data_ultima_atualizacao DATE NOT NULL,
     fk_id_status INT(10) NOT NULL,
-    id_locadora_proprietaria INT(10) NOT NULL,
-    id_locadora_atual INT(10) NOT NULL,
-    foreign key (fk_id_status) references dhlocar.status_veiculo(id_status),
-    foreign key (id_locadora_proprietaria) references dhlocar.locadora(id_locadora),
-    foreign key (id_locadora_atual) references dhlocar.locadora(id_locadora)
+    fk_id_locadora_proprietaria INT(10) NOT NULL,
+    fk_id_locadora_atual INT(10) NOT NULL,
+    foreign key (fk_id_status) references dhlocar.status_veiculos(id),
+    foreign key (id_locadora_proprietaria) references dhlocar.locadoras(id),
+    foreign key (id_locadora_atual) references dhlocar.locadoras(id)
     );
     
-    CREATE TABLE reserva (
-	id_reserva INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    valor_reserva DECIMAL(6,2) NOT NULL,
-    data_criacao_reserva DATETIME NOT NULL,
-    data_retirada_reserva DATETIME NOT NULL,
-	data_entrega_reserva DATETIME NOT NULL,
+    CREATE TABLE reservas (
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    valor DECIMAL(6,2) NOT NULL,
+    data_criacao DATETIME NOT NULL,
+    data_retirada DATETIME NOT NULL,
+	data_entrega DATETIME NOT NULL,
     fk_id_veiculo INT(10) NOT NULL,
     fk_id_usuario INT(10) NOT NULL,
     id_locadora_retirada INT(10) NOT NULL,
     id_locadora_devolucao INT(10) NOT NULL,
-	foreign key (fk_id_usuario) references dhlocar.usuario(id_usuario),
-    foreign key (fk_id_veiculo) references dhlocar.status_veiculo(id_status),
-    foreign key (id_locadora_retirada) references dhlocar.locadora(id_locadora),
-    foreign key (id_locadora_devolucao) references dhlocar.locadora(id_locadora)
+	foreign key (fk_id_usuario) references dhlocar.usuarios(id),
+    foreign key (fk_id_veiculo) references dhlocar.status_veiculos(id),
+    foreign key (id_locadora_retirada) references dhlocar.locadoras(id),
+    foreign key (id_locadora_devolucao) references dhlocar.locadoras(id)
     );
